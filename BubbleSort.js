6b42293f774ae6bcb7dc5765ecd433f6e2593562
HEESU LEE <cutterballer@gmail.com>
2022-04-24 22:31:15 +0900

    Update BubbleSort.js
    //reference zerocho

--- BubbleSort.js
+++ BubbleSort.js
@@ -47,3 +47,19 @@ const swap = (idx1, idx2, arr) => {
     }
     return arr;
   };
+
+//reference zerocho
+var bubbleSort = function(array) {
+  var length = array.length;
+  var i, j, temp;
+  for (i = 0; i < length - 1; i++) { // 순차적으로 비교하기 위한 반복문
+    for (j = 0; j < length - 1 - i; j++) { // 끝까지 돌았을 때 다시 처음부터 비교하기 위한 반복문
+      if (array[j] > array[j + 1]) { // 두 수를 비교하여 앞 수가 뒷 수보다 크면
+        temp = array[j]; // 두 수를 서로 바꿔준다
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+};
