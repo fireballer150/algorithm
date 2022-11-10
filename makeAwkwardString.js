0a9d8335aa76e398d657db470097c01766966397
fireballer150 <cutterballer@gmail.com>
2022-11-11 01:24:47 +0900

    programmers-Lv1-makeAwkwardString

--- makeAwkwardString.js
+++ makeAwkwardString.js
@@ -5,15 +5,15 @@ function solution(s) {
     for (j = 0; j < arr[i].length; j++) {
       if (j % 2 === 0) {
         answer += arr[i][j].toUpperCase();
-        // console.log(
-        //   "j, j%2, arrij,res1",
-        //   j,
-        //   j % 2,
-        //   arr[i][j],
-        //   arr[i][j].toUpperCase(),
-        //   res1,
-        //   arr2
-        // );
+        console.log(
+          "j, j%2, arrij,res1",
+          j,
+          j % 2,
+          arr[i][j],
+          arr[i][j].toUpperCase(),
+          res1,
+          arr2
+        );
       } else {
         answer += arr[i][j].toLowerCase();
       }
